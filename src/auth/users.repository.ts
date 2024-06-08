efceb7733e7847f507f19d1bd28c4929b9099e77
rouzbeh hatamy <r.hatamy@toman.ir>
2024-06-08 18:01:30 +0330

    fix: manage duplicate username

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,7 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDTO } from './DTO/auth-credentials.dto';
+
+enum userErrors {
+  DUPLICATE_USERNAME = '23505',
+}
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
@@ -11,6 +19,14 @@ export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDTO): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === userErrors.DUPLICATE_USERNAME) {
+        throw new ConflictException('username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
